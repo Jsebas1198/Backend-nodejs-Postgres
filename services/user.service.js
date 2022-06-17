0463a976d4bbcb2a916642b9a1281342aca49711
Juan Fernandez <jsebas1198@protonmail.com>
2022-06-17 17:08:31 -0400

    created a db using node-postgres  and pgadmin to interact with it, for this project we use docker, created  libs  folder to intret with thrid party  calls and we use pools for  the request to db

--- services/user.service.js
+++ services/user.service.js
@@ -1,5 +1,6 @@
 const boom = require('@hapi/boom');
 
+const getConnection = require('../libs/postgres');
 class UserService {
   constructor() {}
 
@@ -8,7 +9,9 @@ class UserService {
   }
 
   async find() {
-    return [];
+    const client = await getConnection();
+    const result = await client.query('SELECT * FROM tasks');
+    return result.rows;
   }
 
   async findOne(id) {
