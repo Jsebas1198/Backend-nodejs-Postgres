54135204c031fd27898ec766f8411b2d1bac3320
Juan Fernandez <jsebas1198@protonmail.com>
2022-06-20 15:09:52 -0400

    created a config file to store de values of the conection to the database, install dotenv for the environmental variables

--- libs/postgres.pool.js
+++ libs/postgres.pool.js
@@ -1,11 +1,20 @@
 const { Pool } = require('pg');
 
-const pool = new Pool({
-  host: 'localhost',
-  port: 5432,
-  user: 'juan',
-  password: 'admin123',
-  database: 'my_store',
-});
+const { config } = require('./../config/config');
+
+//for security we enconde the variables
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+//to conect to a remote database using dotenv
+const pool = new Pool({ connectionString: URI });
+// const pool = new Pool({
+//   host: '',
+//   port: ,
+//   user: '',
+//   password: '',
+//   database: '',
+// });
 
 module.exports = pool;
--- services/user.service.js
+++ services/user.service.js
@@ -1,16 +1,28 @@
-const boom = require('@hapi/boom');
+// const boom = require('@hapi/boom');
 
-const getConnection = require('../libs/postgres');
+// const getConnection = require('../libs/postgres');
+
+const pool = require('../libs/postgres.pool');
 class UserService {
-  constructor() {}
+  constructor() {
+    this.pool = pool;
+    this.pool.on('error', (err) => {
+      console.error('Unexpected error on idle client', err);
+      process.exit(-1);
+    });
+  }
 
   async create(data) {
     return data;
   }
 
   async find() {
-    const client = await getConnection();
-    const result = await client.query('SELECT * FROM tasks');
+    // const client = await getConnection();
+    // const result = await client.query('SELECT * FROM tasks');
+    // return result.rows;
+
+    const query = 'SELECT * FROM tasks';
+    const result = await this.pool.query(query);
     return result.rows;
   }
 
