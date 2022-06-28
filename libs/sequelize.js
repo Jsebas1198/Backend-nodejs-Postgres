0d065eb8588c4004ae78336d993076b2ad073535
Juan Fernandez <jsebas1198@protonmail.com>
2022-06-28 15:17:06 -0400

    create .sequelizerrc file for mifrations ocnfigurations, also, create new folders and files on db folder

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -6,13 +6,15 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'postgres',
+  dialect: 'mysql',
   //with login true ir will show us the commands for the query
   logging: true,
 });
 setupModels(sequelize);
+
+//with sequelize.sync() we create tables but is not recommended for production
 sequelize.sync();
 module.exports = sequelize;
